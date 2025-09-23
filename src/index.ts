b5e3f0a60243c50683bc272b5a1f63d53edeee25
khasbilegt <khasbilegt.ts@gmail.com>
2025-09-23 16:54:16 +0800

    update code

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,6 @@ app.get("/", (c) => {
 	return c.text("Hello Hono!");
 });
 
-serve(
-	{
-		fetch: app.fetch,
-		port: 3000,
-	},
-	(info) => {
-		console.log(`Server is running on http://localhost:${info.port}`);
-	},
-);
+serve({ fetch: app.fetch, port: 3000 }, (info) => {
+	console.log(`Server is running on http://localhost:${info.port}`);
+});
